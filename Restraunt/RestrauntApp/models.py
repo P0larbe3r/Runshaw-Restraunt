b45409c7253cb05ad265b729553582fff3c32024
qasimk03 <Qasim12345613@outlook.com>
2025-02-03 10:56:54 +0000

    Menu

--- Restraunt/RestrauntApp/models.py
+++ Restraunt/RestrauntApp/models.py
@@ -13,3 +13,29 @@ class table_booking(models.Model):
     phone=models.IntegerField()
     guests=models.IntegerChoices()
     table_size=models.CharField(40,choices=table_choices)
+
+
+#Class for allergies
+class Allergies(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+
+
+    Category = (
+    ('Main','MAIN'),
+    ('Starter','STARTER'),
+    ('Dessert','DESSERT'),
+    ('Drink','DRINK'),
+)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2,max_digits=4)
+    allergies = models.ManyToManyField(Allergies)
+    category = models.CharField(max_length=7,choices=Category,default='Main')
+
+    def __str__(self):
+        return f'{self.name} - {self.category}'
