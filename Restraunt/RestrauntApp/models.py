bd452a6128289f27b2b644daa0d508f20146fbf6
DylderMort3000 <GOL22204389@student.runshaw.ac.uk>
2025-02-03 10:55:19 +0000

    Restraunt + basics
    Hello i am dylan

--- Restraunt/RestrauntApp/models.py
+++ Restraunt/RestrauntApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class table_booking(models.Model):
+    table_choices={
+        'small':'SMALL',
+        'medium':'MEDIUM',
+        'large':'LARGE'
+    }
+    first_name=models.CharField()
+    last_name=models.CharField()
+    email=models.EmailField()
+    phone=models.IntegerField()
+    guests=models.IntegerChoices()
+    table_size=models.CharField(40,choices=table_choices)
--- Restraunt/RestrauntApp/views.py
+++ Restraunt/RestrauntApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def book_table_view(request):
+    return render(request,"Book_table.html",{})
