cb295eaf93676570e70052939573366de7ead948
Josh <WAT22200001@student.runshaw.ac.uk>
2025-02-05 10:27:09 +0000

    base template

--- Restraunt/RestrauntApp/urls.py
+++ Restraunt/RestrauntApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('book_table',views.book_table_view,name="book_table"),
-    path('Menu/',views.Menu,name="Menu")
+    path('Menu/',views.Menu,name="Menu"),
+    path('contact/', views.contact, name='contact')
 ]
